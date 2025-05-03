56dd32497e1156de0c3dd86167e69d838db613e1
Richard Guilliams <richardlg231@gmail.com>
2025-05-03 13:26:34 -0400

    Added Project Fetching To Front-End

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,16 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-export default function ProjectCard(){
-    const { id } = useParams();
-    const [project, setProject] = useState(null);
-    const [loading, setLoading] = useState(true);
+export default function ProjectCard({title, description, photo, url}){
     return(
-        <a className="card card-link" href="https://www.google.com">
-            <img className="card-image" src="images/richard-guilliams.jpg" width={100} height={100} alt="" />
+        <a className="card card-link" href={url}>
+            <img className="card-image" src={photo} width={100} height={100} alt="" />
             <div className="card-section">
-                <h1 className="card-header">Natours</h1>
-                <p className="card-description">This project was built using MongoDB, Express, Pug and NodeJS. Although it is not a true MERN Stack project. I am proud of it as it represents the completion of my backend course from Jonas Schmedtmann on Udemy and gave me the foundation I needed for my journey into building web apps.</p>
+                <h1 className="card-header">{title}</h1>
+                <p className="card-description">{description}</p>
             </div>
         </a>
     )
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,15 +1,39 @@
+import { useState, useEffect } from "react"
 import ProjectCard from "../components/ProjectCard"
 import Links from "../components/Links"
 import NavBar from "../components/NavBar";
+import { Get } from "../methods/requests"
+
+const DisplayProjects = ({data}) => {
+  return(
+    <div>
+
+      {data.length === 0 ? (<div>No Posts Available...</div>) :
+      (data.map(project => (
+        <div>
+          <ProjectCard title={project.name} description={project.description} photo={`http://localhost:3000/${project.photo}`} url={project.url}/>
+        </div>
+      )))}
+      
+    </div>
+  )
+}
 
 export default function Projects() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {Get('projects', setData, setLoading)}, []);
+
+    if(loading) return <div>Loading...</div>
+
     return (
       <div className="main p-8">
         <NavBar/>
         <div className="main-section">
           <h1 className="main-section-header text-4xl font-bold text-blue-600">My Projects</h1>
           <div className="">
-            <ProjectCard/>
+            <DisplayProjects data={data}/>
           </div>
         </div>
         <Links/>
